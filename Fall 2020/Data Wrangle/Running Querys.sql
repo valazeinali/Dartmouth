0178c8e2291d18533673a23ae3e43c705e9c0510
Vala Zeinali <32586254+valazeinali@users.noreply.github.com>
2020-09-24 16:19:01 -0400

    Add files via upload

--- Fall 2020/Data Wrangle/Running Querys.sql
+++ Fall 2020/Data Wrangle/Running Querys.sql
@@ -36,4 +36,105 @@ select * from vzeinali.parent_avgage
 
 select * from dbo.Demographics
 where try_convert(int,gendercode) = 2
+-------------------------------------------
+-------------------------------------------
+-------------------------------------------
+
+
+
+-- September 24th notes
+
+-- AGG functions
+-------------------------------------------
+select count(contactid) from dbo.Demographics
+
+select count(distinct contactid) from dbo.Demographics
+
+select count(distinct dbo.Demographics.address1_stateorprovince) from dbo.Demographics
+
+select min(tri_age) from dbo.Demographics
+
+select max(tri_age) from dbo.Demographics
+
+-------------------------------------------
+
+select parentcustomeridname,min(tri_age) as Minimum_Age FROM dbo.Demographics
+where tri_age > 40
+group by parentcustomeridname 
+order by Minimum_Age desc
+
+-- same thing but a subset
+select parentcustomeridname,min(tri_age) as Minimum_Age FROM dbo.Demographics
+where tri_age > 40 and try_convert(int, gendercode)=1
+group by parentcustomeridname 
+order by Minimum_Age desc
+
+select parentcustomeridname,min(tri_age) as Minimum_Age FROM dbo.Demographics
+where tri_age > 40 and try_convert(int, gendercode)=2
+group by parentcustomeridname 
+order by Minimum_Age desc
+
+select parentcustomeridname,min(tri_age) as Minimum_Age FROM dbo.Demographics
+where  try_convert(int, gendercode) is null
+group by parentcustomeridname 
+order by Minimum_Age desc
+
+-- same as above
+select parentcustomeridname,min(tri_age) as Minimum_Age FROM dbo.Demographics
+where  gendercode = 'null'
+group by parentcustomeridname 
+order by Minimum_Age desc
+
+-------------------------------------------
+
+select parentcustomeridname,avg(tri_age) as Total_Age from dbo.Demographics 
+group by parentcustomeridname 
+
+-------------------------------------------
+-- Text Mining
+
+select * from dbo.Demographics 
+where parentcustomeridname LIKE 'd%'
+
+SELECT DISTINCT parentcustomeridname from dbo.Demographics
+WHERE parentcustomeridname  like '%hitch%'
+
+select * from dbo.Demographics 
+where try_convert(char,tri_age) LIKE '%4'
+
+-------------------------------------------
+-- JOINS
+
+-- joining two tables and then storing the output into a new table into my folder
+select * into vzeinali.conditions from qbs181.dbo.Demographics d
+inner join 
+dbo.conditions b 
+on
+d.contactid = b.tri_patientid 
+
+
+-------------------------------------------
+
+-- HOMEWORK QUIZ 
+
+-- #1
+select parentcustomeridname,count(tri_name) as hyper_count from vzeinali.conditions
+WHERE parentcustomeridname = 'Dartmouth-Hitchcock' AND tri_name = 'Hypertension' 
+group by parentcustomeridname 
+
+
+-- #2
+select tri_name, avg(tri_age) as avg_age from vzeinali.conditions
+WHERE  tri_name = 'Hypertension' OR tri_name = 'COPD' OR tri_name = 'CHF'
+group by tri_name 
+
+
+-- #3
+select gendercode, tri_name, count(tri_age) as count_it from vzeinali.conditions
+WHERE  (tri_name = 'Hypertension' OR tri_name = 'COPD' OR tri_name = 'CHF') AND (try_convert(int,gendercode) = 2 or 
+try_convert(int,gendercode) = 1)
+group by gendercode, tri_name 
+
+
+-------------------------------------------
 
